c8ff481b1566118b005fe748fd04a7186d59e6c7
Leonardo Magno Marones Lopes <leonardo.marones@mobicare.com.br>
2021-05-27 12:25:19 -0300

    - Implementação do layout da Lista de Alunos.

--- app/src/main/java/alura/com/br/MainActivity.java
+++ app/src/main/java/alura/com/br/MainActivity.java
@@ -2,16 +2,26 @@ package alura.com.br;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.widget.Toast;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-        Toast.makeText(this, "Alex Felipe", Toast.LENGTH_LONG).show();
+        ListView listaDeAlunos = findViewById(R.id.activity_main_lista_de_alunos);
+
+        List<String> alunos = new ArrayList<>(Arrays.asList("Alex", "Fran", "José", "Maria", "Ana"));
+
+        listaDeAlunos.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, alunos));
     }
 }
